485fc1ce4f3d3c1da7c595f4e17ac658a6a547a1
BulzEye <shirgaonkarom@yahoo.in>
2022-11-06 21:48:55 +0530

    fixed rename buttons and most of rename mode bugs

--- gd-frontend/src/components/File.js
+++ gd-frontend/src/components/File.js
@@ -1,53 +1,103 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import "./File.css";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const File = (props) => {
 
     const [menuOpen, setMenuOpen] = useState(false);
     const [rmenuOpen, setRMenuOpen] = useState(false);
     const [rmenuPos, setRMenuPos] = useState([0, 0]);
-    const [renameMode, _setRenameMode] = useState(false);
-    const renameModeRef = useRef(renameMode);
-    const [fileName, setFileName] = useState({initial: props.info.name, current: props.info.name});
-    const textBox = useRef(null);
+    const [renameMode, setRenameMode] = useState(false);
+    const [fileName, _setFileName] = useState({initial: props.info.name, current: props.info.name});
 
-    const setRenameMode = (val) => {
-        console.log(`val passed to function; ${val}`);
-        renameModeRef.current = val;
-        console.log(`rename mode ref modified: ${renameModeRef.current}`);
-        _setRenameMode(val);
+    const fileNameRef = useRef(fileName);
+
+    const ref = useOutsideClick((e) => {
+        // console.log(e.target);
+        // console.log(textBox.current);
+        setMenuOpen(false);
+        // console.log(e.target)
+        setRMenuOpen(false);
+        // console.log(textBox.current);
+        // if(renameModeRef.current && (e.target !== textBox.current)) {
+        //     console.log("yems rename");
+        //     renameFile(e);
+        // }
+    });
+
+    const refText = useOutsideClick((e) => {
+        // if(renameMode) {
+            exitRenameMode(e);
+        // }
+    });
+
+    
+    // function that ensures setFileName changes value of state as well as that of similarly named ref 
+    // (trick to ensure value of state does not freeze inside setEventListener)
+    const setFileName = (val) => {
+        // console.log(`val passed to function; ${val}`);
+        fileNameRef.current = val;
+        // console.log(`rename mode ref modified: ${fileNameRef.current}`);
+        _setFileName(val);
     }
-
+    
     const menuOpenClose = (e) => {
         // console.log(e);
         setMenuOpen(!menuOpen);
-        e.stopPropagation();
+        // e.stopPropagation();
     }
 
-    useEffect(() => {
-        window.addEventListener("click", (e) => {
-            // console.log(e.target);
-            // console.log(textBox.current);
-            setMenuOpen(false);
-            setRMenuOpen(false);
-            console.log(`Rename Mode inside event listener: ${renameMode}`);
-            console.log(`RenameModeRef: ${renameModeRef.current}`);
-            console.log(e.target)
-            console.log(textBox.current);
-            if(renameModeRef.current && (e.target !== textBox.current)) {
-                console.log("yems rename");
-                renameFile(e);
-            }
-        });
-    }, []);
 
-    const renameFile = (e) => {
+    const exitRenameMode = (e) => {
+        console.log("Exiting rename mode");
+
         // prevent default behaviour of submitting form 
         // (the rename textbox 'form' does not have a submit button, but pressing
         // enter after changing the name defaults to submitting the form)
         e.preventDefault();
-
         
+        // console.log(`File Name inside event listener: ${fileName}`);
+        // console.log(`FileNameRef: ${fileNameRef.current}`);
+
+        console.log(`Initial file name: ${fileName.initial}`);
+        console.log(`Current file name: ${fileName.current}`);
+        console.log(`FileRef file name: ${fileNameRef.current.current}`);
+
+        setRenameMode(false);
+        
+        // if filename has actually been changed
+        if(fileNameRef.current.current !== fileName.initial) {
+            alert("File name changed");
+            console.log(fileName.current);
+
+            // TODO: first make sure there is no duplicate file by sending appropriate GET request
+            
+            // Send rename request to server
+            
+            fetch(`/renamefile`, {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({
+                    id: props.info._id,
+                    newFileName: fileNameRef.current.current
+                })
+            })
+            .then((res) => {
+                if(res.error) {
+                    console.log(`ERROR: ${res.error}`);
+                }
+                console.log("Renamed file");
+    
+                // set 'initial' file name to be the new name
+                setFileName({
+                    ...fileNameRef.current,
+                    initial: fileNameRef.current.current
+                });
+            })
+        }
+    };
+
+    const enterRenameMode = (e) => {  
         // close side menu
         setMenuOpen(false);
         
@@ -59,42 +109,10 @@ const File = (props) => {
         // from inside.
         // TL;DR: we use ref cuz regular state doesn't work with window event listener
         
-        console.log(`Rename mode: ${renameModeRef.current}`);
-        setRenameMode(!(renameModeRef.current));
+        setRenameMode(true);
         
-        // e.stopPropagation();
+        e.stopPropagation();
 
-        console.log(`Initial file name: ${fileName.initial}`);
-        console.log(`Current file name: ${fileName.current}`);
-        
-        // if filename has actually been changed
-        if(fileName.current !== fileName.initial) {
-            alert("File name changed");
-            console.log(fileName.current);
-            
-            // Send rename request to server
-            
-            fetch(`/renamefile`, {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({
-                    id: props.info._id,
-                    newFileName: fileName.current
-                })
-            })
-            .then((res) => {
-                if(res.error) {
-                    console.log(`ERROR: ${res.error}`);
-                }
-                console.log("Renamed file");
-
-                // set 'initial' file name to be the new name
-                setFileName({
-                    ...fileName,
-                    initial: fileName.current
-                });
-            })
-        }
     }
 
     const deleteFile = () => {
@@ -122,7 +140,7 @@ const File = (props) => {
             setRMenuOpen(true);
         }
         }>
-            <form onSubmit={renameFile}>
+            <form onSubmit={exitRenameMode}>
             <div className="fileName">
                 <span className="nameText">
                     {!renameMode && fileName.current}
@@ -132,7 +150,7 @@ const File = (props) => {
                     name="fileName" 
                     id="fileNameInput" 
                     value={fileName.current}
-                    ref={textBox}
+                    ref={refText}
                     onChange={(e) => {setFileName({...fileName, current: e.target.value})}}
                 />}
                 <span className="nameText">
@@ -144,11 +162,11 @@ const File = (props) => {
                 {props.info.type}
             </div> */}
             <div className="fileMenu">
-                <i className="bi bi-three-dots-vertical" onClick={menuOpenClose}></i>
+                <i className="bi bi-three-dots-vertical" ref={ref} onClick={menuOpenClose}></i>
                 {/* <i className="material-icons">more_vert</i> */}
                 {menuOpen && <div className="menu" onClick={(e) => e.preventDefault()}>
                     <ul>
-                        <li onClick={renameFile}>Rename</li>
+                        <li onClick={enterRenameMode}>Rename</li>
                         <hr />
                         <li onClick={deleteFile}>Delete</li>
                     </ul>
